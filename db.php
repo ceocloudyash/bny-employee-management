7849469c605c1a2066b4816afe71a7e0d94dc341
ceocloudyash <vedanshdonu@gmail.com>
2026-06-11 12:44:36 +0530

    Connect Railway MySQL

--- db.php
+++ db.php
@@ -1,10 +1,10 @@
 <?php
 
-$host = getenv("DB_HOST");
-$user = getenv("DB_USER");
-$pass = getenv("DB_PASS");
-$db   = getenv("DB_NAME");
-$port = getenv("DB_PORT");
+$host = getenv('MYSQLHOST');
+$user = getenv('MYSQLUSER');
+$pass = getenv('MYSQLPASSWORD');
+$db   = getenv('MYSQLDATABASE');
+$port = getenv('MYSQLPORT');
 
 $conn = new mysqli(
     $host,
@@ -14,9 +14,7 @@ $conn = new mysqli(
     $port
 );
 
-if($conn->connect_error)
-{
-    die("Database Connection Failed: " . $conn->connect_error);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
-
 ?>
