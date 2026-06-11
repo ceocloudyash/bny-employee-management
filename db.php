caed409848d548563f7895626e7a4f8da2fd294a
ceocloudyash <vedanshdonu@gmail.com>
2026-06-11 13:53:18 +0530

    Debug Railway env

--- db.php
+++ db.php
@@ -1,26 +1,7 @@
 <?php
 
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
-$host = getenv('MYSQLHOST');
-$user = getenv('MYSQLUSER');
-$pass = getenv('MYSQLPASSWORD');
-$db   = getenv('MYSQLDATABASE');
-$port = getenv('MYSQLPORT');
-
 echo "<pre>";
-echo "HOST: $host\n";
-echo "USER: $user\n";
-echo "DB: $db\n";
-echo "PORT: $port\n";
+print_r($_ENV);
 echo "</pre>";
 
-$conn = new mysqli($host, $user, $pass, $db, $port);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-echo "Database Connected Successfully";
-?>
+exit();
