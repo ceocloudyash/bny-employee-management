fa4b41199efad4081a0f9a1c8f02c0886838f345
ceocloudyash <vedanshdonu@gmail.com>
2026-06-11 13:57:39 +0530

    debug env vars

--- db.php
+++ db.php
@@ -4,4 +4,25 @@ echo "<pre>";
 print_r($_ENV);
 echo "</pre>";
 
-exit();
+exit();<?php
+
+echo "<pre>";
+
+echo "GETENV HOST = ";
+var_dump(getenv('DB_HOST'));
+
+echo "\n\n_ENV HOST = ";
+var_dump($_ENV['DB_HOST'] ?? null);
+
+echo "\n\n_SERVER HOST = ";
+var_dump($_SERVER['DB_HOST'] ?? null);
+
+echo "\n\nAll ENV:\n";
+print_r($_ENV);
+
+echo "\n\nAll SERVER:\n";
+print_r($_SERVER);
+
+echo "</pre>";
+
+exit;
